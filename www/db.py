2a400c7045449e5df46542ee8e39729aebb0a9c1
ftong <ftong@163.com>
2017-12-15 22:52:01 +0800

    init database awesome tables

---
+++ www/db.py
@@ -0,0 +1,21 @@
+#!/usr/bin/evn python3
+# _*_ conding: utf-8 _*_
+
+__author__ = 'Ftong Tong'
+
+'''
+init database: awesome
+'''
+
+import orm
+from models import User, Blog, Comment
+
+def test():
+	yield from orm.create_pool(user = 'www-data', password='www-data', database = 'awesome')
+	
+	u = User(name = 'Test', email = 'test@example.com', passwd = '1234567890', image = 'about:blank')
+
+	yield from u.save()
+
+for x in test():
+	pass
