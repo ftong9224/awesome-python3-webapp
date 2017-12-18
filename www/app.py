b0282b1d8bdcdf2442774dede44a05fa043cd1cb
ftong <ftong@163.com>
2017-12-18 21:52:41 +0800

    The 10th day is OK

--- www/app.py
+++ www/app.py
@@ -15,8 +15,12 @@ from datetime import datetime
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 
+from config import configs
+
 import orm
 from coroweb import add_routes, add_static
+
+from handlers import cookie2user, COOKIE_NAME
 
 def init_jinja2(app, **kw):
 	logging.info('init jinja2...')
@@ -39,29 +43,52 @@ def init_jinja2(app, **kw):
 			env.filters[name] = f
 	app['__templating__'] = env
 
-async def logger_factory(app, handler):
-	async def logger(request):
+@asyncio.coroutine
+def logger_factory(app, handler):
+	@asyncio.coroutine
+	def logger(request):
 		logging.info('Request: %s %s' % (request.method, request.path))
 		# await asyncio.sleep(0.3)
-		return(await handler(request))
+		return(yield from handler(request))
 	return logger
 
-async def data_factory(app, handler):
-	async def parse_data(request):
+@asyncio.coroutine
+def auth_factory(app, handler):
+	@asyncio.coroutine
+	def auth(request):
+		logging.info('check user: %s %s' % (request.method, request.path))
+		request.__user__ = None
+		cookie_str = request.cookies.get(COOKIE_NAME)
+		if cookie_str:
+			user = yield from cookie2user(cookie_str)
+			if user:
+				logging.info('set current user: %s' % user.email)
+				request.__user__ = user
+		if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+			return web.HTTPFound('/signin')
+		return (yield from handler(request))
+	return auth
+
+@asyncio.coroutine
+def data_factory(app, handler):
+	@asyncio.coroutine
+	def parse_data(request):
 		if request.method == 'POST':
 			if request.content_type.startswith('application/json'):
-				request.__data__ = await request.json()
+				request.__data__ = yield from request.json()
 				logging.info('request json: %s' % str(request.__data__))
 			elif request.content_type.startswith('application/x-www-from-urlencoded'):
-				request.__data__ = await request.post()
+				request.__data__ = yield from request.post()
 				logging.info('request from: %s' % str(request.__data__))
-		return ( await handler(request))
+		return (yield from handler(request))
 	return parse_data
 
-async def response_factory(app, handler):
-	async def response(request):
+@asyncio.coroutine
+def response_factory(app, handler):
+	@asyncio.coroutine
+	def response(request):
 		logging.info('Response handler...')
-		r = await handler(request)
+		r = yield from handler(request)
 		if isinstance(r, web.StreamResponse):
 			return r
 		if isinstance(r, bytes):
@@ -81,6 +108,7 @@ async def response_factory(app, handler):
 				resp.content_type = 'application/json;charset=utf-8'
 				return resp
 			else:
+				r['__user__'] = request.__user__
 				resp = web.Response(body = app['__templating__'].get_template(template).render(**r).encode('utf-8'))
 				resp.content_type = 'text/html;charset=utf-8'
 				return resp
@@ -109,29 +137,17 @@ def datetime_filter(t):
 	dt = datetime.fromtimestamp(t)
 	return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
-'''
-def index(request):
-	return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
-
-async def init(loop):
-	app = web.Application(loop=loop)
-	app.router.add_route('GET', '/', index)
-	srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-	logging.info('server started at http://127.0.0.1:9000...')
-	return srv
-'''
-
 @asyncio.coroutine
 def init(loop):
 	yield from orm.create_pool(loop, host = '127.0.0.1', port = 3306, user = 'www-data', password = 'www-data', database = 'awesome')
 	app = web.Application(loop = loop, middlewares = [
-		logger_factory, response_factory
+		logger_factory, auth_factory, response_factory
 	])
 	init_jinja2(app, filters = dict(datetime = datetime_filter))
 	add_routes(app, 'handlers')
 	add_static(app)
-	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-	logging.info('server started at http://127.0.0.1:9000...')
+	srv = yield from loop.create_server(app.make_handler(), '0.0.0.0', 9000)
+	logging.info('server started at http://106.14.214.217.0.0.1:9000...')
 	return srv
 
 loop =  asyncio.get_event_loop()
